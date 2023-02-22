ce91c491bfabda88346ea1e8cca72c6d8db571ed
AnamikaBajpai <anamikaagnihotri12@gmail.com>
2023-02-22 12:44:24 +0530

    commit done

--- dom.js
+++ dom.js
@@ -76,5 +76,24 @@ li[2].style.backgroundColor = "green";
  }
 
 
+//QUERYSELECTOR-----------
 
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom ='solid 4px #ccc';
+
+// var input = document.querySelector("input");
+// input.value = "Hello World";
+
+// var submit = document.querySelector('input[type = "submit"]');
+// submit.value = "SEND";
+
+// var item = document.querySelector('.list-group-item');
+// itemm.style.color ='red';
+
+// var lastItem = document.querySelector('.list-group-item: last-child');
+// lastItem.style.color ='blue';
+
+// var secondItem = document.querySelector('.list-group-item: nth-child(2)');
+// secondItem.style.color ='coral';
  
